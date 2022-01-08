61a5ef112054a5e95e709746e67b66f03046a8a6
Adito213 <adito213@gmail.com>
2022-01-08 23:18:30 +0200

    [UPDATE]Project's skeleton

---
+++ src/test/java/com/SushiEmperor/SushiEmperor/SushiEmperorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SushiEmperor.SushiEmperor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiEmperorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
